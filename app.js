8824de5a9ce1b7d7c50c617f2ee5eacdb0e7bffb
dallinhumphrey <dallinhumphrey@gmail.com>
2020-05-19 16:51:31 -0600

    studying JS

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-var car = {
-  make: "volvo",
-  speed: 160,
-  engine: {
-    size: 2.0,
-    make: "bmw",
-    fuel: "gas",
-    pistons: [{ maker: "BMW" }, { maker: "BMW2" }],
-  },
-  drive: function () {
-    return "dive";
-  },
-};
+function name() {
+  var fullname = "Dallin Humphrey";
+  var lastname = "Parkes";
 
-var array = [
-  "string",
-  100,
-  ["embed", 200],
-  { car: "ford" },
-  function () {
-    return "drive";
-  },
-];
-console.log(car.make);
+  function concat(name) {
+    return "MR." + name;
+  }
+
+  return concat(fullname);
+}
+
+var obj = [{}];
+
+// var car = {
+//   make: "volvo",
+//   speed: 160,
+//   engine: {
+//     size: 2.0,
+//     make: "bmw",
+//     fuel: "gas",
+//     pistons: [{ maker: "BMW" }, { maker: "BMW2" }],
+//   },
+//   drive: function () {
+//     return "dive";
+//   },
+// };
+
+// var array = [
+//   "string",
+//   100,
+//   ["embed", 200],
+//   { car: "ford" },
+//   function () {
+//     return "drive";
+//   },
+// ];
+
+// console.log(car.make);
+// console.log(car.engine.make);
+// console.log(car.engine.pistons[0].maker);
+// console.log(car.engine.pistons[1].maker);
+// console.log(array[2]);
